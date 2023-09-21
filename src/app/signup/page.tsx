f6a39a48d171a0d75041737250e37530867c7bd6
Tk-brainCodes <ikhuohoneboreimethankgod@gmail.com>
2023-09-21 15:13:07 +0100

    chore: fixed console errors

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -32,7 +32,7 @@ const Signup = (props: Props) => {
           <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 '>
             <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
               <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl '>
-                Sign in to your account
+                Sign up to your account
               </h1>
               <form className='space-y-4 md:space-y-6' onSubmit={signUp}>
                 <div>
